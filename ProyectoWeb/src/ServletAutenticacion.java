1d6eda13bfb58e6a01cf74686888aebc5dada0ae
Jpetis <jaimepetis@gmail.com>
2015-06-25 21:37:54 +0200

    Commit inicial

--- ProyectoWeb/src/ServletAutenticacion.java
+++ ProyectoWeb/src/ServletAutenticacion.java
@@ -22,6 +22,15 @@ import org.apache.logging.log4j.Logger;
 public class ServletAutenticacion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final Logger log = LogManager.getRootLogger();
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 	
 	/* (non-Javadoc)
 	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
